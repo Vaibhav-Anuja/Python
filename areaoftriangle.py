a326ee1c6a25201dc272674f1c867df104256dd0
Vaibhav-Anuja <91534003+Vaibhav-Anuja@users.noreply.github.com>
2021-11-13 23:27:35 +0530

    Add files via upload

---
+++ areaoftriangle.py
@@ -0,0 +1,23 @@
+"""
+base = int( input("enter base"))
+height = int (input("enter height"))
+def area(base,height):
+        a = base*height*0.5
+        return a
+
+b = area(base,height)
+print(b)
+"""
+
+""" write a program that counts total number of digits in given number """
+"""
+a = int(input("Enter a number"))
+def size(n):
+    count = 0
+    while n>1:
+            count+=1
+            n=n/10
+    return count
+num_count=size(a)
+print("num_count= %d" %num_count)
+"""
